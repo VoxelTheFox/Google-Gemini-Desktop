c96e8d0399ec8e729167b7e5517eaea2f2ae1fe0
Voxel <voxelthefox@outlook.com>
2026-02-06 22:42:00 -0500

    Add autostart checkbox to tray menu and improve tray icon handling

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,11 @@
 use tauri::{
-    menu::{Menu, MenuItem},
+    menu::{Menu, MenuItem, CheckMenuItem}, // Added CheckMenuItem
     tray::{MouseButton, TrayIconBuilder, TrayIconEvent},
     Manager,
     image::Image,
 };
 use tauri_plugin_autostart::MacosLauncher;
+use tauri_plugin_autostart::ManagerExt; // Required to access .autolaunch()
 use tauri_plugin_global_shortcut::{GlobalShortcutExt, ShortcutState};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -18,7 +19,6 @@ pub fn run() {
         ))
         .plugin(
             tauri_plugin_global_shortcut::Builder::new()
-                // FIX 1: Added underscore to `_shortcut` to fix warning
                 .with_handler(|app, _shortcut, event| {
                     if event.state() == ShortcutState::Pressed {
                         let window = app.get_webview_window("main").unwrap();
@@ -40,16 +40,32 @@ pub fn run() {
             #[cfg(target_os = "macos")]
             let tray_icon = Image::from_bytes(include_bytes!("../icons/tray.png")).unwrap();
 
-            // FIX 2: Use a PNG for Windows (ICO causes the crash!)
-            // We use the 32x32.png that Tauri generates by default
             #[cfg(not(target_os = "macos"))]
             let tray_icon = Image::from_bytes(include_bytes!("../icons/32x32.png")).unwrap();
 
-            // FIX 3: Removed `mut` because Windows doesn't need to modify the builder
-            let tray_builder = TrayIconBuilder::new()
+            // --- CREATE MENU ITEMS ---
+            let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+            let toggle_i = MenuItem::with_id(app, "toggle", "Show/Hide", true, None::<&str>)?;
+            
+            // 1. Create the Checkbox Item
+            let autostart_i = CheckMenuItem::with_id(app, "autostart", "Run on Startup", true, false, None::<&str>)?;
+
+            // 2. Check current state and update checkbox
+            let autostart_manager = app.autolaunch();
+            if autostart_manager.is_enabled().unwrap_or(false) {
+                autostart_i.set_checked(true)?;
+            }
+
+            // 3. Add to Menu
+            let menu = Menu::with_items(app, &[&toggle_i, &autostart_i, &quit_i])?;
+
+            // --- TRAY SETUP ---
+            #[allow(unused_mut)]
+            let mut tray_builder = TrayIconBuilder::new()
+                .menu(&menu)
                 .icon(tray_icon)
                 .show_menu_on_left_click(false)
-                .on_menu_event(|app, event| match event.id.as_ref() {
+                .on_menu_event(move |app, event| match event.id.as_ref() {
                     "quit" => app.exit(0),
                     "toggle" => {
                         let window = app.get_webview_window("main").unwrap();
@@ -58,6 +74,17 @@ pub fn run() {
                         } else {
                             window.show().unwrap();
                             window.set_focus().unwrap();
+                        }
+                    }
+                    "autostart" => {
+                        // 4. Handle the Toggle Logic
+                        let manager = app.autolaunch();
+                        if manager.is_enabled().unwrap_or(false) {
+                            let _ = manager.disable();
+                            autostart_i.set_checked(false).unwrap();
+                        } else {
+                            let _ = manager.enable();
+                            autostart_i.set_checked(true).unwrap();
                         }
                     }
                     _ => {}
@@ -76,20 +103,12 @@ pub fn run() {
                     _ => {}
                 });
 
-            // --- MACOS SPECIFIC ---
-            // We have to use a separate block for the mutable logic to avoid the warning on Windows
             #[cfg(target_os = "macos")]
-            let tray_builder = tray_builder.icon_as_template(true);
+            {
+                tray_builder = tray_builder.icon_as_template(true);
+            }
 
-            // --- CREATE MENU ---
-            let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
-            let toggle_i = MenuItem::with_id(app, "toggle", "Show/Hide", true, None::<&str>)?;
-            let menu = Menu::with_items(app, &[&toggle_i, &quit_i])?;
-
-            // Finish building
-            tray_builder
-                .menu(&menu)
-                .build(app)?;
+            tray_builder.build(app)?;
 
             // --- GLOBAL SHORTCUT ---
             #[cfg(target_os = "macos")]
